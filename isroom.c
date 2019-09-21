55e4fbfae479e3b7a49fcd65aa12808ff05dbd02
Amory lorch Romny weaver <aromny-w@il-k4.21-school.ru>
2019-09-21 22:12:25 +0300

    lem-in

--- isroom.c
+++ isroom.c
@@ -12,28 +12,52 @@
 
 #include "lem_in.h"
 
-int	isroom(char *line)
+static int	iscoord2(char **line)
 {
-	int	name;
-	int	x;
-	int	y;
+	long	nbr;
 
-	name = 0;
-	x = 0;
-	y = 0;
-	if (*line == 'L' || *line == '#')
+	nbr = 0;
+	if (**line == '-')
 		return (0);
-	while ((ft_isalnum(*line) || *line == '_') && (name = 1))
-		line++;
-	if (*line == ' ')
-		line++;
-	while (ft_isdigit(*line) && (x = 1))
-		line++;
-	if (*line == ' ')
-		line++;
-	while (ft_isdigit(*line) && (y = 2))
-		line++;
-	if (!*line && (name && x && y))
+	while (ft_isdigit(**line) && nbr <= INT_MAX)
+		nbr = 10 * nbr + (*(*line)++ - '0');
+	if (**line == '\0' && nbr <= INT_MAX)
 		return (1);
 	return (0);
+}
+
+static int	iscoord1(char **line)
+{
+	long	nbr;
+
+	nbr = 0;
+	if (**line == '-')
+		return (0);
+	while (ft_isdigit(**line) && nbr <= INT_MAX)
+		nbr = 10 * nbr + (*(*line)++ - '0');
+	if (*(*line)++ == ' ' && nbr <= INT_MAX)
+		return (1);
+	return (0);
+}
+
+static int	isname(char **line)
+{
+	while (ft_isalnum(**line) || **line == '_')
+		(*line)++;
+	if (*(*line)++ == ' ')
+		return (1);
+	return (0);
+}
+
+int	isroom(char *line)
+{
+	if (*line == 'L' || *line == '#')
+		return (0);
+	if (!isname(&line))
+		return (0);
+	if (!iscoord1(&line))
+		return (0);
+	if (!iscoord2(&line))
+		return (0);
+	return (1);
 }
